8596f1d458b5e800416d4f0ed57cdc833520f57f
Robert van Reems <rreems@didacticum.nl>
2020-04-11 21:58:18 +0200

    add my first files

---
+++ kladblokRVR.ps1
@@ -0,0 +1,10 @@
+# variables
+$ResourceGroupName = 'RVR.PersonalDSC'
+$TemplatePath = Get-Location | Join-Path -ChildPath 'ARMTemplates'
+
+# Create new resource group
+New-AzResourceGroup -Name $ResourceGroupName -Tag @{project='RVR.PersonalDSC'} -Location 'westeurope'
+
+# Deploy Automation account
+New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $(Join-Path -Path $TemplatePath -ChildPath 'AutomationAccount.json')
+
