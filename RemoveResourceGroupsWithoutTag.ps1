19a0e7be04a01a441724e83a06c64877ec3eb362
Robert van Reems <rreems@didacticum.nl>
2020-04-12 16:44:37 +0200

    upd remove script add module

--- RemoveResourceGroupsWithoutTag.ps1
+++ RemoveResourceGroupsWithoutTag.ps1
@@ -1,5 +1,26 @@
+# Copied from https://docs.microsoft.com/nl-nl/azure/automation/automation-first-runbook-textual-powershell
+# Ensures you do not inherit an AzContext in your runbook
+Disable-AzContextAutosave –Scope Process
+
+$connection = Get-AutomationConnection -Name AzureRunAsConnection
+
+while(!($connectionResult) -And ($logonAttempt -le 10))
+{
+    $LogonAttempt++
+    # Logging in to Azure...
+    $connectionResult =    Connect-AzAccount `
+                               -ServicePrincipal `
+                               -Tenant $connection.TenantID `
+                               -ApplicationId $connection.ApplicationID `
+                               -CertificateThumbprint $connection.CertificateThumbprint
+
+    Start-Sleep -Seconds 30
+}
+
+## Now find and remove the resource groups
 # Get all the resource groups
-$colResGroup = Get-AzureRmResourceGroup
+$colResGroup = Get-AZResourceGroup
 
 # remove the resource groups
-$colResGroup | Where-Object {$_.Tags -eq $null} | Remove-AzureRmResourceGroup -Force 
+$colResGroup 
+# todo
